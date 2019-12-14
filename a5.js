76aa190e50978694c086ec82ec2720a2486512da
arlogoldman <54850964+arlogoldman@users.noreply.github.com>
2019-12-14 15:12:54 -0500

    Add files via upload

---
+++ a5.js
@@ -0,0 +1,21 @@
+var move1 = document.getElementById('leftn');
+crossbar.addEventListener("mouseover", Animation);
+function Animation() {
+}
+move1.addEventListener("mouseup", RemoveAddClass);
+function RemoveAddClass() {
+}
+
+
+
+
+var move2 = document.getElementById('e3');
+
+move2.addEventListener("mouseover", Animation);
+function Animation() {
+}
+move2.addEventListener("mouseup", RemoveAddClass);
+function RemoveAddClass() {
+
+
+}
